a360441fda483802b4c446ed289be6b9685f18b9
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2020-01-06 18:29:37 +0200

    Second commit

--- Ex3/src/gameClient/FruitInterface.java
+++ Ex3/src/gameClient/FruitInterface.java
@@ -1,5 +1,11 @@
 package gameClient;
 
-public interface FruitInterface {
-
+public interface FruitInterface 
+{
+	/**
+	 * Each fruit has to be on an edge, hence it can't be "floating" fruit
+	 * The fruits can be neither apple or banana.
+	 * If the robot goes from a lower key of node to a bigger key, and there is an apple, the robot will take the apple and the apple will change the position to a different edge randomly.
+	 * If the robot goes from a bigger key of node to a lower key, and there is a banana, the robot will take the banana and the banana will change the position to a different edge randomly.
+	 */
 }
